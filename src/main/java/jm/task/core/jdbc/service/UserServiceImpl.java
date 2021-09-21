eb62ce2a9fde81cd4bdfaa26a2f24ed0bec2e39c
Valentin-java <v_tin@mail.ru>
2021-09-21 14:18:14 +0300

    First ver.

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,31 +1,114 @@
 package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
-import java.util.List;
+import javax.persistence.criteria.CriteriaBuilder;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
 
 public class UserServiceImpl implements UserService {
-    public void createUsersTable() {
 
+
+    public UserServiceImpl() throws SQLException, ClassNotFoundException {
+    }
+
+    public void createUsersTable() {
+        try (Connection conn = Util.getMySQLConnection()) {
+            String sql = "CREATE TABLE `dbtest`.`users`(`id` INT NOT NULL AUTO_INCREMENT, `name` VARCHAR(45) NULL, `lastName` VARCHAR(45) NULL, `age` INT NULL, PRIMARY KEY (`id`)) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8 COLLATE = utf8_unicode_ci";
+            PreparedStatement cmd = conn.prepareStatement(sql);
+            cmd.executeUpdate();
+        } catch (SQLException throwables) {
+
+        }
+        catch (ClassNotFoundException e) {
+
+        }
     }
 
     public void dropUsersTable() {
 
+        try (Connection conn = Util.getMySQLConnection()){
+            String sql = "DROP TABLE `dbtest`.`users`";
+            PreparedStatement cmd = conn.prepareStatement(sql);
+            cmd.executeUpdate();
+        } catch (SQLException throwables) {
+
+        }
+        catch (ClassNotFoundException e) {
+
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        try (Connection conn = Util.getMySQLConnection()) {
+            String sql = "INSERT INTO users (name, lastName, age) VALUES (?,?,?)";
+            PreparedStatement cmd = conn.prepareStatement(sql);
+            cmd.setString(1, name);
+            cmd.setString(2, lastName);
+            cmd.setByte(3, age);
+
+            if (cmd.executeUpdate() == 1) {
+                System.out.println("User с именем – " + name + " добавлен в базу данных.");
+            }
+
+        } catch (SQLException throwables) {
+
+        }
+        catch (ClassNotFoundException e) {
+
+        }
 
     }
 
     public void removeUserById(long id) {
+        try (Connection conn = Util.getMySQLConnection()) {
+            String sql = "DELETE FROM `users` WHERE id = " + id;
+            PreparedStatement cmd = conn.prepareStatement(sql);
+            cmd.executeUpdate();
+        } catch (SQLException throwables) {
 
+        }
+        catch (ClassNotFoundException e) {
+
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+            List<User> list = new ArrayList<>();
+        try (Connection conn = Util.getMySQLConnection()) {
+            String sql = "SELECT * FROM users";
+            PreparedStatement cmd = conn.prepareStatement(sql);
+            ResultSet result = cmd.executeQuery();
+
+            while (result.next()) {
+                String name = result.getString("name");
+                String lastName = result.getString("lastName");
+                byte age = result.getByte("age");
+                list.add(new User(name, lastName, age));
+            }
+        } catch (SQLException throwables) {
+
+        }
+        catch (ClassNotFoundException e) {
+
+        }
+        return list;
     }
 
     public void cleanUsersTable() {
+        try (Connection conn = Util.getMySQLConnection()) {
+            String sql = "TRUNCATE `dbtest`.`users`";
+            PreparedStatement cmd = conn.prepareStatement(sql);
+            cmd.executeUpdate();
+        } catch (SQLException throwables) {
 
+        }
+        catch (ClassNotFoundException e) {
+
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,23 @@
 package jm.task.core.jdbc.util;
 
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+
+    public static final String DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
+    public static final String CONNECTION_STRING = "jdbc:mysql://localhost/dbtest?useSSL=false";
+    public static final String USER_NAME = "root";
+    public static final String PASSWORD = "root";
+
+
+    public static Connection getMySQLConnection()
+            throws SQLException, ClassNotFoundException {
+        Class.forName(DRIVER_NAME);
+        return DriverManager.getConnection(CONNECTION_STRING, USER_NAME, PASSWORD);
+    }
+
 }
